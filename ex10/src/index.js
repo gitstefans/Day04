cebc111cd96807a16f96a69d96318db850d7e8b7
Stefan Skočo <stefansgit@outlook.com>
2021-03-11 15:01:44 +0100

    Decalre String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+  
+  // Only change code below this line
+    var myFirstName = "Stefan";
+    var myLastName = "Skoco";
+    var myHomeTown = "Pale";
+  // Only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+  };
+}
+
+console.log(main());
+module.exports = main;
