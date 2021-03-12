c3f35dcb8db5fe08ba7303a68037500ede53772b
Stefan Skočo <stefansgit@outlook.com>
2021-03-12 01:18:53 +0100

    Dividing One Number by Another

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,6 +1,6 @@
 function main(numOne, numTwo){
     var quotient;
-    if(numTwo===0){
+    if(numTwo === 0){
       quotient = 'Divide by 0! Cannot divide by zero!';
     }
     else{
@@ -11,9 +11,9 @@ function main(numOne, numTwo){
     return quotient;
 }
 
-console.log(main(15,3));        //change this line
-console.log(main(25,5));
-console.log(main(420,10));
-console.log(main(55,0));
+console.log(main(15, 3));        //change this line
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
 
 module.exports = main;
