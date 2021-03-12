1fe0ebe6e3ccc7ddd230f220b8d3b5adfff69c3a
Stefan Skočo <stefansgit@outlook.com>
2021-03-12 01:44:41 +0100

    Compound assignment with augmented A and S

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -5,9 +5,9 @@ function main(){
     var myY = 19;
     var myZ = 24;
 
-    myX+= 3;
-    myY+= 17;
-    myZ-= 20;
+    myX += 3;
+    myY += 17;
+    myZ -= 20;
 
   // Only change code above this line
   return {
